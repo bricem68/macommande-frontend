60cd23939f1ef8e755e15a479ba3234f82e24228
bricem68 <brice.meyer@hotmail.fr>
2025-06-20 16:03:42 +0200

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+
+document.getElementById("root").innerHTML = `
+  <h1>Bienvenue sur Ma Commande Groupée</h1>
+  <p>Choisissez votre rôle pour accéder à votre espace :</p>
+  <ul>
+    <li><a href="#">Acheteur</a></li>
+    <li><a href="#">Vendeur</a></li>
+    <li><a href="#">Administrateur</a></li>
+  </ul>
+`;
